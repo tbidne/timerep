72339afbea14d9a13b852101e8b02faccc955b70
Tommy Bidne <tbidne@protonmail.com>
2022-04-23 01:23:40 +1200

    Take off -Weverything
    Having an unrecognized warning is an error, so we can't really use
    -Weverything and hope to be compatible across multiple GHC versions.
    
    Also fix nix CI hash.

--- nix/ghc_base.nix
+++ nix/ghc_base.nix
@@ -3,10 +3,11 @@
 }:
 
 let
+  #nixpkgs_2 because that's the main nixpkgs in our flake.
   pkgs = import
     (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
-      sha256 = lock.nodes.nixpkgs.locked.narHash;
+      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs_2.locked.rev}.tar.gz";
+      sha256 = lock.nodes.nixpkgs_2.locked.narHash;
     })
     { };
   compiler = pkgs.haskell.packages."${compilerVersion}";
