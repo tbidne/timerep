2339934856e3ad9b1f9516ae2dba2ffa598fbee3
Tommy Bidne <tbidne@protonmail.com>
2022-12-07 17:07:28 +1300

    Swap flake-utils for flake-parts, update deps

--- flake.nix
+++ flake.nix
@@ -6,20 +6,20 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # haskell
     algebra-simple = {
       url = "github:tbidne/algebra-simple";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     bounds = {
       url = "github:tbidne/bounds";
       inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -27,44 +27,48 @@
     { algebra-simple
     , bounds
     , flake-compat
-    , flake-utils
+    , flake-parts
     , nixpkgs
     , self
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      devTools = c: with c; [
-        ghcid
-        haskell-language-server
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv: withDevTools:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "relative-time";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if withDevTools then devTools compiler else [ ]));
-          overrides = final: prev: with compiler; {
-            algebra-simple =
-              final.callCabal2nix "algebra-simple" algebra-simple { };
-            bounds = final.callCabal2nix "bounds" bounds { };
-            tasty-hedgehog = prev.tasty-hedgehog_1_3_1_0;
-          };
+    flake-parts.lib.mkFlake { inherit self; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          devTools = c: with c; [
+            ghcid
+            haskell-language-server
+          ];
+          ghc-version = "ghc924";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv: withDevTools:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "relative-time";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if withDevTools then devTools compiler else [ ]));
+              overrides = final: prev: with compiler; {
+                algebra-simple =
+                  final.callCabal2nix "algebra-simple" algebra-simple { };
+                bounds = final.callCabal2nix "bounds" bounds { };
+                tasty-hedgehog = prev.tasty-hedgehog_1_3_1_0;
+              };
+            };
+        in
+        {
+          packages.default = mkPkg false false;
+          devShells.default = mkPkg true true;
+          devShells.ci = mkPkg true false;
         };
-    in
-    {
-      packages.default = mkPkg false false;
-      devShells.default = mkPkg true true;
-      devShells.ci = mkPkg true false;
-    });
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
