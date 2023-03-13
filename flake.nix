2ccf3c97b7d8926ecae01a5b1dca4eddcec54ea7
Tommy Bidne <tbidne@protonmail.com>
2023-03-14 11:29:42 +1300

    Add windows to CI

--- flake.nix
+++ flake.nix
@@ -11,13 +11,13 @@
 
     # haskell
     algebra-simple = {
-      url = "github:tbidne/algebra-simple";
+      url = "github:tbidne/algebra-simple/windows";
       inputs.flake-compat.follows = "flake-compat";
       inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     bounds = {
-      url = "github:tbidne/bounds";
+      url = "github:tbidne/bounds/windows";
       inputs.flake-compat.follows = "flake-compat";
       inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
