57b80df91e1f83bbc832c072eeacd6dffcd13987
Tommy Bidne <tbidne@protonmail.com>
2023-05-11 17:01:10 +1200

    Simplify ci

--- README.md
+++ README.md
@@ -3,11 +3,7 @@
 # relative-time
 
 [![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/tbidne/relative-time?include_prereleases&sort=semver)](https://github.com/tbidne/relative-time/releases/)
-![haskell](https://img.shields.io/static/v1?label=&message=9.6&logo=haskell&logoColor=655889&labelColor=2f353e&color=655889)
+[![ci](http://img.shields.io/github/actions/workflow/status/tbidne/relative-time/ci.yaml?branch=main)](https://github.com/tbidne/relative-time/actions/workflows/ci.yaml)
 [![MIT](https://img.shields.io/github/license/tbidne/relative-time?color=blue)](https://opensource.org/licenses/MIT)
-
-[![nix](http://img.shields.io/github/actions/workflow/status/tbidne/relative-time/nix.yaml?branch=main&label=nix&logo=nixos&logoColor=85c5e7&labelColor=2f353c)](https://github.com/tbidne/relative-time/actions/workflows/nix.yaml)
-[![cabal](http://img.shields.io/github/actions/workflow/status/tbidne/relative-time/cabal.yaml?branch=main&label=cabal&labelColor=2f353c)](https://github.com/tbidne/relative-time/actions/workflows/cabal.yaml)
-[![style](http://img.shields.io/github/actions/workflow/status/tbidne/relative-time/style.yaml?branch=main&label=style&logoColor=white&labelColor=2f353c)](https://github.com/tbidne/relative-time/actions/workflows/style.yaml)
 
 </div>
--- flake.nix
+++ flake.nix
@@ -36,16 +36,24 @@
         let
           buildTools = c: [
             c.cabal-install
-            pkgs.gnumake
             pkgs.zlib
           ];
           devTools = c: [
-            (hlib.dontCheck c.ghcid)
+            (hlib.dontCheck c.apply-refact)
+            (hlib.dontCheck c.cabal-fmt)
             (hlib.dontCheck c.haskell-language-server)
+            (hlib.dontCheck c.hlint)
+            (hlib.dontCheck c.ormolu)
+            pkgs.nixpkgs-fmt
           ];
           ghc-version = "ghc944";
           compiler = pkgs.haskell.packages."${ghc-version}".override {
             overrides = final: prev: {
+              algebra-simple = final.callCabal2nix "algebra-simple" algebra-simple { };
+              apply-refact = prev.apply-refact_0_11_0_0;
+              bounds = final.callCabal2nix "bounds" bounds { };
+              hedgehog = prev.hedgehog_1_2;
+              tasty-hedgehog = prev.tasty-hedgehog_1_4_0_0;
               # https://github.com/ddssff/listlike/issues/23
               ListLike = hlib.dontCheck prev.ListLike;
             };
@@ -60,18 +68,43 @@
                 pkgs.haskell.lib.addBuildTools drv
                   (buildTools compiler ++
                     (if returnShellEnv then devTools compiler else [ ]));
-              overrides = final: prev: with compiler; {
-                algebra-simple =
-                  final.callCabal2nix "algebra-simple" algebra-simple { };
-                bounds = final.callCabal2nix "bounds" bounds { };
-                hedgehog = prev.hedgehog_1_2;
-                tasty-hedgehog = prev.tasty-hedgehog_1_4_0_0;
-              };
             };
+          mkApp = drv: {
+            type = "app";
+            program = "${drv}/bin/${drv.name}";
+          };
         in
         {
           packages.default = mkPkg false;
           devShells.default = mkPkg true;
+
+          apps = {
+            format = mkApp (
+              pkgs.writeShellApplication {
+                name = "format";
+                text = builtins.readFile ./tools/format.sh;
+                runtimeInputs = [
+                  compiler.cabal-fmt
+                  compiler.ormolu
+                  pkgs.nixpkgs-fmt
+                ];
+              }
+            );
+            lint = mkApp (
+              pkgs.writeShellApplication {
+                name = "lint";
+                text = builtins.readFile ./tools/lint.sh;
+                runtimeInputs = [ compiler.hlint ];
+              }
+            );
+            lint-refactor = mkApp (
+              pkgs.writeShellApplication {
+                name = "lint-refactor";
+                text = builtins.readFile ./tools/lint-refactor.sh;
+                runtimeInputs = [ compiler.apply-refact compiler.hlint ];
+              }
+            );
+          };
         };
       systems = [
         "x86_64-darwin"
