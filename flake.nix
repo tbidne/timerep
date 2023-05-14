a46177bb8c0b3d7311ab294b07efc6adb613a7ae
Tommy Bidne <tbidne@protonmail.com>
2023-05-14 13:59:07 +1200

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-hs-utils = {
+      url = "github:tbidne/nix-hs-utils";
+      inputs.flake-compat.follows = "flake-compat";
+    };
 
     # haskell
     algebra-simple = {
@@ -24,86 +28,51 @@
     };
   };
   outputs =
-    inputs@{ algebra-simple
-    , bounds
-    , flake-compat
+    inputs@{ flake-compat
     , flake-parts
-    , nixpkgs
+    , nix-hs-utils
     , self
+    , ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       perSystem = { pkgs, ... }:
         let
-          buildTools = c: [
-            c.cabal-install
-            pkgs.zlib
-          ];
-          devTools = c: [
-            (hlib.dontCheck c.apply-refact)
-            (hlib.dontCheck c.cabal-fmt)
-            (hlib.dontCheck c.haskell-language-server)
-            (hlib.dontCheck c.hlint)
-            (hlib.dontCheck c.ormolu)
-            pkgs.nixpkgs-fmt
-          ];
           ghc-version = "ghc944";
           compiler = pkgs.haskell.packages."${ghc-version}".override {
             overrides = final: prev: {
-              algebra-simple = final.callCabal2nix "algebra-simple" algebra-simple { };
               apply-refact = prev.apply-refact_0_11_0_0;
-              bounds = final.callCabal2nix "bounds" bounds { };
               hedgehog = prev.hedgehog_1_2;
               tasty-hedgehog = prev.tasty-hedgehog_1_4_0_0;
               # https://github.com/ddssff/listlike/issues/23
               ListLike = hlib.dontCheck prev.ListLike;
-            };
+            } // nix-hs-utils.mkLibs inputs final [
+              "algebra-simple"
+              "bounds"
+            ];
           };
           hlib = pkgs.haskell.lib;
           mkPkg = returnShellEnv:
-            compiler.developPackage {
-              inherit returnShellEnv;
+            nix-hs-utils.mkHaskellPkg {
+              inherit compiler pkgs returnShellEnv;
               name = "relative-time";
               root = ./.;
-              modifier = drv:
-                pkgs.haskell.lib.addBuildTools drv
-                  (buildTools compiler ++
-                    (if returnShellEnv then devTools compiler else [ ]));
             };
-          mkApp = drv: {
-            type = "app";
-            program = "${drv}/bin/${drv.name}";
-          };
+          hs-dirs = "src test";
         in
         {
           packages.default = mkPkg false;
           devShells.default = mkPkg true;
 
           apps = {
-            format = mkApp (
-              pkgs.writeShellApplication {
-                name = "format";
-                text = builtins.readFile ./tools/format.sh;
-                runtimeInputs = [
-                  compiler.cabal-fmt
-                  compiler.ormolu
-                  pkgs.nixpkgs-fmt
-                ];
-              }
-            );
-            lint = mkApp (
-              pkgs.writeShellApplication {
-                name = "lint";
-                text = builtins.readFile ./tools/lint.sh;
-                runtimeInputs = [ compiler.hlint ];
-              }
-            );
-            lint-refactor = mkApp (
-              pkgs.writeShellApplication {
-                name = "lint-refactor";
-                text = builtins.readFile ./tools/lint-refactor.sh;
-                runtimeInputs = [ compiler.apply-refact compiler.hlint ];
-              }
-            );
+            format = nix-hs-utils.format {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint = nix-hs-utils.lint {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint-refactor = nix-hs-utils.lint-refactor {
+              inherit compiler hs-dirs pkgs;
+            };
           };
         };
       systems = [
