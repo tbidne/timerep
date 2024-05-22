b3be9b3b904059671ef07dbcbb11b8c04d50ddde
Tommy Bidne <tbidne@protonmail.com>
2024-05-23 00:23:00 +1200

    Update nix

--- flake.nix
+++ flake.nix
@@ -21,48 +21,46 @@
     };
   };
   outputs =
-    inputs@{ flake-parts
-    , nix-hs-utils
-    , self
-    , ...
+    inputs@{
+      flake-parts,
+      nix-hs-utils,
+      self,
+      ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
-          ghc-version = "ghc962";
+          ghc-version = "ghc982";
           compiler = pkgs.haskell.packages."${ghc-version}".override {
-            overrides = final: prev: {
-              hedgehog = prev.hedgehog_1_3;
-              hlint = prev.hlint_3_6_1;
-              ormolu = prev.ormolu_0_7_1_0;
-            } // nix-hs-utils.mkLibs inputs final [
-              "algebra-simple"
-              "bounds"
-            ];
+            overrides =
+              final: prev:
+              { }
+              // nix-hs-utils.mkLibs inputs final [
+                "algebra-simple"
+                "bounds"
+              ];
           };
           hlib = pkgs.haskell.lib;
-          mkPkg = returnShellEnv:
+          mkPkg =
+            returnShellEnv:
             nix-hs-utils.mkHaskellPkg {
               inherit compiler pkgs returnShellEnv;
               name = "relative-time";
               root = ./.;
             };
-          hsDirs = "src test";
+          compilerPkgs = {
+            inherit compiler pkgs;
+          };
         in
         {
           packages.default = mkPkg false;
           devShells.default = mkPkg true;
 
           apps = {
-            format = nix-hs-utils.format {
-              inherit compiler hsDirs pkgs;
-            };
-            lint = nix-hs-utils.lint {
-              inherit compiler hsDirs pkgs;
-            };
-            lint-refactor = nix-hs-utils.lint-refactor {
-              inherit compiler hsDirs pkgs;
-            };
+            format = nix-hs-utils.format compilerPkgs;
+            lint = nix-hs-utils.lint compilerPkgs;
+            lintRefactor = nix-hs-utils.lintRefactor compilerPkgs;
           };
         };
       systems = [
